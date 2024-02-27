0e335a02bff8a65b851b685b5f0bf945ab6f2d87
Abhinav-k-p <146640328+Abhinav-k-p@users.noreply.github.com>
2024-02-27 16:14:04 +0530

    Update 18_.py

--- pythontask/18_.py
+++ pythontask/18_.py
@@ -10,3 +10,7 @@ def second_largest(numbers):
         elif second_largest is None or num > second_largest:
             second_largest = num
     return second_largest
+
+my_numbers = [10, 5, 8, 20, 15]
+result = second_largest(my_numbers)
+print(f"The second largest number is: {result}")
