9e3ef2b37449653dad11c19d3007efa180f1787b
Abhinav-k-p <146640328+Abhinav-k-p@users.noreply.github.com>
2024-03-19 21:09:17 +0530

    3_.py

--- pyhon assesment 2/3_.py
+++ pyhon assesment 2/3_.py
@@ -1,11 +1,17 @@
 # Search for a String in a Text File
 
 file_path = 'testing.txt'
-search_string = 'C'
+search_string = 'beinex'
+
+found = False  # Flag to track if the search string is found in any line
 
 with open(file_path, 'r') as file:
     for line_number, line in enumerate(file, start=1):
         if search_string in line:
             print(f"Found '{search_string}' in line {line_number}: {line.strip()}")
-        else:
-            print("This character not found")
+            found = True
+            # No need to continue searching if the string is found once
+            break
+
+if not found:
+    print(f"String '{search_string}' not found in the file.")
